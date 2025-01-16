150a99dc9f97159341c112bc704374b1f319f855
donjne <davidjrn247@gmail.com>
2025-01-16 10:13:40 +0100

    a7

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -112,7 +112,7 @@ const AnimatedSphere = () => {
         <div className="absolute inset-0 rounded-full overflow-hidden">
           {/* Static base image */}
           <img 
-            src="/sidecat.png" 
+            src="/sideguy.png" 
             alt="Sphere" 
             className="w-full h-full object-cover"
           />
